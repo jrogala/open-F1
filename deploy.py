35fa11bbe38928800236177101f0d49cdc8c1c4c
Jimmy <jimmy.rogala@ens-rennes.fr>
2023-02-20 17:48:09 +0100

    commit debug (creating docs onthefly)

--- deploy.py
+++ deploy.py
@@ -4,6 +4,8 @@ from traitlets.config.loader import Config
 import nbformat as nbf
 from nbconvert.exporters import HTMLExporter
 from nbconvert.preprocessors import TagRemovePreprocessor
+import os
+import logging
 
 # Setup config
 c = Config()
@@ -23,11 +25,14 @@ html_exporter = HTMLExporter(config=c)
 html_exporter.register_preprocessor(TagRemovePreprocessor(config=c), True)
 
 body, resources = html_exporter.from_filename("src/index.ipynb")
-with open(f"docs/index.html", "w") as f:
+os.mkdir("./docs")
+with open(f"./docs/index.html", "w") as f:
+    logging.debug("Writing index.html")
     f.write(body)
 all_filePath = glob.glob("src/gp/*/*/*.ipynb")
 for filePath in all_filePath:
     uniqueName = filePath.replace("/", "").replace(".ipynb", "").replace("src", "")
     body, resources = html_exporter.from_filename(filePath)
     with open(f"docs/{uniqueName}.html", "w") as f:
+        logging.debug(f"Writing {filePath}")
         f.write(body)
