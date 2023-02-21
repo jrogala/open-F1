d65d0722843e4fd317a67d39e70cfea364c0a804
Jimmy <jimmy.rogala@ens-rennes.fr>
2023-02-21 09:26:45 +0100

    changed display to show starting tyres

--- src/modules/display.py
+++ src/modules/display.py
@@ -8,6 +8,18 @@ def grid(session):
     sortedDriversindex = sorted(np.arange(len(drivers)), key=lambda x: positions[x])
     sortedDrivers = np.array(drivers)[sortedDriversindex]
     text = ""
+    startingTyre = session.laps.query("LapNumber == 1").loc[:, ["Driver", "Compound"]].set_index("Driver")
+    startingTyre["Driver Name"] = [session.get_driver(name)["FullName"] for name in startingTyre.index]
     for i in range(0, len(drivers)-2, 2):
-        text += f" {sortedDrivers[i]}    |---|\n|---|    {sortedDrivers[i+1]}\n"
-    print("|---|        \n" + text + f" {sortedDrivers[-2]}    |---|\n         {sortedDrivers[-1]}")
+        text += f" {sortedDrivers[i]}    |-{startingTyre.loc[sortedDrivers[i+1]]['Compound'][0]}-|\n|-{startingTyre.loc[sortedDrivers[i+2]]['Compound'][0]}-|    {sortedDrivers[i+1]}\n"
+    print(f"|-{startingTyre.loc[sortedDrivers[0]]['Compound'][0]}-|        \n" + text + f" {sortedDrivers[-2]}    |-{startingTyre.loc[sortedDrivers[-1]]['Compound'][0]}-|\n         {sortedDrivers[-1]}")
+
+
+if __name__ == "__main__":
+    import logging
+    import fastf1
+    session = fastf1.get_session(2022, 'bahrain', 'R')
+    session.load(telemetry=False, laps=True, weather=False)
+    session.load_telemetry()
+    logging.warn("Debug")
+    grid(session)
